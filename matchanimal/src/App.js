5553ac6cb46870e16bd71ab23cff63b5630f2165
Fathima Minha <139671210+fathiminha@users.noreply.github.com>
2024-11-22 12:17:54 +0000

    create table layout

--- matchanimal/src/App.js
+++ matchanimal/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Animal from './components/Animal';
 
 function App() {
   return (
     <div className="App">
-       
+       <Animal />
     </div>
   );
 }
--- matchanimal/src/components/Animal.js
+++ matchanimal/src/components/Animal.js
@@ -1,7 +1,20 @@
 export default function Animal(){
     return(
         <>
-           
+           <div> 
+            <table border='1'>
+                <thead>
+                    <tr>
+                        <td colspan='3'> ANIMAL MATCHING GAME </td>
+                    </tr>
+                    <tr>
+                        <td> Result </td>
+                        <td> Animal Name </td>
+                        <td> Select the animal </td>
+                    </tr>
+                </thead>
+            </table>
+           </div>
         </>
     )
 }
